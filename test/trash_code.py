0f55bac504d8c29ea7802c319e84001c5329973c
Dani Fdez <danifdezalvarez@gmail.com>
2017-10-12 16:49:02 +0200

    Close #18 and works on #16, DiGraph parser added

--- test/trash_code.py
+++ test/trash_code.py
@@ -11,6 +11,7 @@ from classrank_io.graph.parsers.ttl_simple_digraph_parser import TtlSimpleDigrap
 from classrank_io.graph.yielders.ttl_simple_triples_yielder import TtlSimpleTriplesYielder
 from classrank_io.classpointers.formatters.one_per_line_classpointers_formatter import OnePerLineClasspointerFormatter
 from classpointer_candidates_finder import CpCandidatesFinder
+from classrank_io.graph.parsers.ttl_full_digraph_parser import TtlFullDigraphParser
 
 #
 # parser = TsvSpoGraphParser("files\\tsv_spo_tiny.tsv")
@@ -44,46 +45,46 @@ from classpointer_candidates_finder import CpCandidatesFinder
 #
 
 ########################################### DBPEDIA
-
-db_es_path = "C:\\Users\\Dani\\Documents\\EII\\doctorado\\PAPERS_PROPIOS\\classrank_dbpedia\\datasets\\mappingbased_objects_en_uris_es.ttl\\mappingbased_objects_en_uris_es.ttl"
-db_en_path = "C:\\Users\\Dani\\Documents\\EII\\doctorado\\PAPERS_PROPIOS\\classrank_dbpedia\\datasets\\mappingbased_objects_en.ttl\\db_en.ttl"
-
-parser = TtlSimpleDigraphParser(db_en_path)
-
-formater = RawPageRankFormatter()
-
-# pageranker = PageRanker(graph_parser=parser,
-#                         pagerank_formatter=formater,
-#                         damping_factor=0.85)
-# result = pageranker.generate_pagerank()
-
-
-classpointers_parser = OnePerLineClasspointerParser("files\\dbpedia_cps_tiny.tsv")
-classpointer_set = classpointers_parser.parse_classpointers()
-classpointer_str = RawClasspointerFormater().format_classpointers_set(classpointer_set)
-
-
-triple_yielder = TtlSimpleTriplesYielder(source_file=db_en_path)
-classrank_formater = SortedJsonFormatedInterface(target_file="files\\out\\CR_dbp_en_tiny.json")
-
-classranker = ClassRanker(digraph_parser=parser,
-                          triple_yielder=triple_yielder,
-                          classpointers_parser=classpointers_parser,
-                          classrank_formatter=classrank_formater,
-                          damping_factor=0.9,
-                          class_security_threshold=20,
-                          instantiation_security_threshold=20,
-                          max_edges=500000,
-                          max_iter_pagerank=250)
-
-result_2 = classranker.generate_classrank()
-
-print result_2
-print classranker.triples_analized
-print classranker.triples_with_error
-print "--------"
-print classranker.number_of_classes
-print classranker.number_of_entities
+#
+# db_es_path = "C:\\Users\\Dani\\Documents\\EII\\doctorado\\PAPERS_PROPIOS\\classrank_dbpedia\\datasets\\mappingbased_objects_en_uris_es.ttl\\mappingbased_objects_en_uris_es.ttl"
+# db_en_path = "C:\\Users\\Dani\\Documents\\EII\\doctorado\\PAPERS_PROPIOS\\classrank_dbpedia\\datasets\\mappingbased_objects_en.ttl\\db_en.ttl"
+#
+# parser = TtlSimpleDigraphParser(db_en_path)
+#
+# formater = RawPageRankFormatter()
+#
+# # pageranker = PageRanker(graph_parser=parser,
+# #                         pagerank_formatter=formater,
+# #                         damping_factor=0.85)
+# # result = pageranker.generate_pagerank()
+#
+#
+# classpointers_parser = OnePerLineClasspointerParser("files\\dbpedia_cps_tiny.tsv")
+# classpointer_set = classpointers_parser.parse_classpointers()
+# classpointer_str = RawClasspointerFormater().format_classpointers_set(classpointer_set)
+#
+#
+# triple_yielder = TtlSimpleTriplesYielder(source_file=db_en_path)
+# classrank_formater = SortedJsonFormatedInterface(target_file="files\\out\\CR_dbp_en_tiny.json")
+#
+# classranker = ClassRanker(digraph_parser=parser,
+#                           triple_yielder=triple_yielder,
+#                           classpointers_parser=classpointers_parser,
+#                           classrank_formatter=classrank_formater,
+#                           damping_factor=0.9,
+#                           class_security_threshold=20,
+#                           instantiation_security_threshold=20,
+#                           max_edges=500000,
+#                           max_iter_pagerank=250)
+#
+# result_2 = classranker.generate_classrank()
+#
+# print result_2
+# print classranker.triples_analized
+# print classranker.triples_with_error
+# print "--------"
+# print classranker.number_of_classes
+# print classranker.number_of_entities
 
 
 
@@ -100,4 +101,13 @@ print classranker.number_of_entities
 #                                        classpointers_formater=OnePerLineClasspointerFormatter(target_file="files\\out\\db_en_cps.tsv"),
 #                                        class_security_threshold=15)
 # print candidates_finder.generate_classpointer_candidates()
+
+
+############### Full TTl parsers
+
+parser1 = TtlFullDigraphParser(source_file="files\\sample_ttl_full_tiny.ttl")
+graph1 = parser1.parse_graph()
+for an_edge in graph1.edges_iter():
+    print an_edge
+
 
