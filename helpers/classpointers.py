1bbd7d24c24865fce16c78f0c49baf5b9a9d6a72
Dani Fdez <danifdezalvarez@gmail.com>
2017-11-09 14:45:09 +0100

    Close #42, close #43 and close #44

--- helpers/classpointers.py
+++ helpers/classpointers.py
@@ -4,6 +4,7 @@ from classrank_io.graph.yielders.tsv_spo_triples_yielder import TsvSpoTriplesYie
 from classrank_io.graph.yielders.ttl_explicit_spo_triples_yielder import TtlExplicitSpoTriplesYielder
 from classrank_io.graph.yielders.ttl_full_triples_yielder import TtlFullTriplesYielder
 from classrank_io.graph.yielders.ttl_simple_triples_yielder import TtlSimpleTriplesYielder
+from classrank_io.graph.yielders.ttl_full_memory_kind_triples_yielder import TtlFullMemoryKindTriplesYielder
 
 from classrank_io.classpointers.formatters.one_per_line_classpointers_formatter import OnePerLineClasspointerFormatter
 from classrank_io.classpointers.formatters.raw_classpointer_formatter import RawClasspointerFormater
@@ -13,12 +14,15 @@ from helpers.const import *
 _ACCEPTED_GRAPH_FORMATS = [TTL_FULL_FORMAT, TSV_SPO_FORMAT, TTL_SIMPLE_FORMAT, TTL_EXPLICIT_SPO_FORMAT]
 
 
-def _build_yielder(graph_format, graph_file, raw_graph):
+def _build_yielder(graph_format, graph_file, raw_graph, save_memory_mode):
     if raw_graph is not None and graph_format in [TTL_FULL_FORMAT, TTL_SIMPLE_FORMAT, TTL_EXPLICIT_SPO_FORMAT]:
         return TtlFullTriplesYielder(string_graph=raw_graph)
     else:
         if graph_format == TTL_FULL_FORMAT:
-            return TtlFullTriplesYielder(source_file=graph_file)
+            if save_memory_mode:
+                return TtlFullMemoryKindTriplesYielder(source_file=graph_file)
+            else:
+                return TtlFullTriplesYielder(source_file=graph_file)
         elif graph_format == TTL_EXPLICIT_SPO_FORMAT:
             return TtlExplicitSpoTriplesYielder(source_file=graph_file)
         elif graph_format == TTL_SIMPLE_FORMAT:
@@ -56,7 +60,8 @@ def _assert_valid_param_combination(class_security_threshold, graph_format, raw_
 
 
 def generate_classpointer_candidates(class_security_threshold=15, graph_format=TTL_FULL_FORMAT,
-                                     raw_graph=None, graph_file=None, output_file=None, string_return=False):
+                                     raw_graph=None, graph_file=None, output_file=None, string_return=False,
+                                     save_memory_mode=False):
     _assert_valid_param_combination(class_security_threshold=class_security_threshold,
                                     graph_format=graph_format,
                                     raw_graph=raw_graph,
@@ -65,7 +70,7 @@ def generate_classpointer_candidates(class_security_threshold=15, graph_format=T
                                     string_return=string_return)
 
     formater = _build_formatter(graph_file, string_return)
-    yielder = _build_yielder(graph_format, graph_file, raw_graph)
+    yielder = _build_yielder(graph_format, graph_file, raw_graph, save_memory_mode)
 
     cp_finder = CpCandidatesFinder(triple_yielder=yielder,
                                    classpointers_formater=formater,
