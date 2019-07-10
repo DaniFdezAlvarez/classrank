663d93db36755293b4aabc378e92e5e200446879
Dani Fdez <danifdezalvarez@gmail.com>
2019-07-10 13:59:36 +0200

    Works on #87. Parse prefixes on each query, to be used with the default ones.

--- experimentation/query_mining/class_usage_miner.py
+++ experimentation/query_mining/class_usage_miner.py
@@ -1,6 +1,9 @@
 import re
 
-_REGEX_PREFIX = re.compile("(PREFIX)|(prefix) +[\w\-]* *: *<.+>")
+# _REGEX_PREFIX = re.compile("(PREFIX)|(prefix) +[\w\-]* *: *<.+>")
+_REGEX_PREFIX = re.compile("PREFIX", flags=re.IGNORECASE)
+
+_REGEX_TYPE_QUERY = re.compile("(^|[ \n]+)((SELECT)|(ASK)|(CONSTRUCT)|(DESCRIBE)|(select)|(ask)|(construct)|(describe))[ \n]+")
 
 class ClassUsageMiner(object):
 
@@ -10,8 +13,7 @@ class ClassUsageMiner(object):
         self._entities_yielder_func = self._set_internal_yielder_func()
         self._classes_total_mentions = self._turn_set_of_classes_into_zeros_dict(set_target_classes)
 
-
-        self._namespaces
+        self._default_namespaces = namespaces
 
 
     def _turn_set_of_classes_into_zeros_dict(self, target_set):
@@ -30,16 +32,46 @@ class ClassUsageMiner(object):
 
     def _mine_entries(self):
         for an_entry in self._entities_yielder_func():
-            prefixes_dict = self._parse_new_prefixes(an_entry)
-            uri_mentions = self._detect_class_mentions(an_entry)
+            index_type_of_query = self._detect_index_type_of_query(an_entry)
+            if index_type_of_query != -1:
+                new_prefixes_dict = self._parse_new_prefixes(an_entry.str_query[:index_type_of_query])
+                uri_mentions = self._detect_uri_mentions(an_entry.str_query[:index_type_of_query])
 
 
-    def _detect_uri_mentions(self, an_entry):
-        return self._detect_complete_uri_mentions
+    def _detect_index_type_of_query(self, an_entry):
+        res = re.search(_REGEX_TYPE_QUERY, an_entry.str_query)
+        return -1 if res is None else res.start()
 
 
-    def _parse_new_prefixes(self, an_entry):
-        pass # TODO
+    def _detect_uri_mentions(self, str_query):
+        return self._detect_complete_uri_mentions() + self._dectect_prefixed_uri_mentions()
+
+    def _detect_complete_uri_mentions(self):
+        pass  # TODO
+
+    def _dectect_prefixed_uri_mentions(self):
+        pass  # TODO
+
+
+    def _parse_new_prefixes(self, str_prefixes_list):
+        if len(str_prefixes_list) < 11:  # len("prefix : <>")
+            return {}
+        pieces = re.split(_REGEX_PREFIX, str_prefixes_list)
+        if len(pieces) < 2:  # The first piece does not contain a nampespace, it is an (probably empty string) prior to the first PREFIX keyword
+            return {}
+        result = {}
+        for a_piece in pieces:
+            index_end_prefix = a_piece.find(":")  # First ':' will be the ':' used after the prefix
+            prefix = a_piece[:index_end_prefix].strip()
+
+            index_beg_uri = a_piece.find("<") + 1
+            index_end_uri = a_piece.find(">")
+
+            result[prefix] = a_piece[index_beg_uri:index_end_uri]
+        return result
+
+
+
 
 
 
