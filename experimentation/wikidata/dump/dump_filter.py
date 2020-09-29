c730b065f84fe20f50e4a29d91763f6bbb004891
Dani Fdez <danifdezalvarez@gmail.com>
2020-09-29 21:43:50 +0200

    Working on #122. New ijson parser and skeleton for tirple filters

---
+++ experimentation/wikidata/dump/dump_filter.py
@@ -0,0 +1,18 @@
+class DirectedGraphDumpFilter(object):
+     def __init__(self, source_file, target_file):
+         pass
+
+
+class DirectedGraphSlicerDumpFilter(object):
+    def __init__(self):
+        pass
+
+
+class ClassCountDumpFilter(object):
+    def __init__(self, source_file, target_file, classpointers):
+        pass
+
+
+class MultiDumpFilter(object):
+    def __init__(self, dump_filters):
+        pass
