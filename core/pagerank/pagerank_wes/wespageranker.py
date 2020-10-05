5ee20510afd61033273ba890cf809a160cd114da
Dani Fdez <danifdezalvarez@gmail.com>
2020-10-05 21:57:13 +0200

    Minor fixes

--- core/pagerank/pagerank_wes/wespageranker.py
+++ core/pagerank/pagerank_wes/wespageranker.py
@@ -5,7 +5,7 @@ from core.pagerank.pagerank_wes.power_iterator import PowerIterator
 class Wespageranker(object):
 
     def __init__(self, source_file=None, raw_graph=None, damping_factor=0.85,
-                 epsilon=1.0e-8, max_iters=300, max_edges=-1, base_yielder=None):
+                 epsilon=1.0e-8, max_iters=300, max_edges=-1, base_triples_yielder=None, base_edges_yielder=None):
         self._source_file = source_file
         self._raw_graph = raw_graph,
         self._damping_factor = damping_factor
@@ -13,14 +13,16 @@ class Wespageranker(object):
         self._max_iters = max_iters
         self._iterations_performd = 0
         self._max_edges = max_edges
-        self._base_yielder = base_yielder
+        self._base_yielder = base_triples_yielder
+        self._base_edges_yielder = base_edges_yielder
 
     def compute_pagerank_vector(self):
         matrix = SMatrix(d=1 - self._damping_factor,
                          source_file=self._source_file,
                          raw_graph=self._raw_graph,
                          max_edges=self._max_edges,
-                         base_triple_yielder=self._base_yielder)
+                         base_triple_yielder=self._base_yielder,
+                         base_edges_yielder=self._base_edges_yielder)
 
         p_iterator = PowerIterator(target_matrix=matrix,
                                    epsilon=self._eps,
