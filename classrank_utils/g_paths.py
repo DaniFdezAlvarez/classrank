2ca5ea7c3e5220cc7de38f6e08bcc5310641736a
Dani Fdez <danifdezalvarez@gmail.com>
2020-10-30 14:05:33 +0100

    Working on #148

--- classrank_utils/g_paths.py
+++ classrank_utils/g_paths.py
@@ -1,13 +1,16 @@
 import networkx as nx
+import multiprocessing as mp
 
 _S = 0
 _O = 2
+
 
 def build_graph_for_paths(triples_yielder):
     result = nx.DiGraph()
     for a_triple in triples_yielder.yield_triples():
         result.add_edge(a_triple[_S], a_triple[_O])
     return result
+
 
 def shortest_path(graph, origin=None, destination=None):
     """
@@ -22,3 +25,41 @@ def shortest_path(graph, origin=None, destination=None):
     return nx.shortest_path(graph,
                             source=origin,
                             target=destination)
+
+
+MAX_AVAILABLE = -1
+
+
+class EfficientShortPathCalculator(object):
+
+    def __init__(self, nx_graph, n_threads=MAX_AVAILABLE):
+        self._g = nx_graph
+        self._s_paths_dict = None
+        self._n_threads = n_threads if n_threads != MAX_AVAILABLE else mp.cpu_count()
+
+
+    def get_shortest_path_between_nodes(self, origin, dest):
+        if self._s_paths_dict is None:
+            self._compute_shortest_paths()
+        if origin in self._s_paths_dict and dest in self._s_paths_dict[origin]:
+            return self._s_paths_dict[origin][dest]
+        return None
+
+    def get_shortests_paths(self):
+        if self._s_paths_dict is None:
+            self._compute_shortest_paths()
+        return self._s_paths_dict
+
+    def _shortest_path_from_a_node(self, a_node):
+        return nx.shortest_path(self._g,
+                                source=a_node,
+                                target=None)
+
+    def _compute_shortest_paths(self):
+        pool = mp.Pool(self._n_threads)
+        result = {a_node: pool.apply(self._shortest_path_from_a_node, args=a_node) for a_node in self._g.nodes}
+        pool.close()
+        return result
+
+
+
